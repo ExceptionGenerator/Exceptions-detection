1d5d59d00ae71711d9749af4ea1ee10e63e36cf7
Shivmohan <nshivmohan08@gmail.com>
2022-11-08 16:48:40 +0530

    Queue implementation using generics

---
+++ Detect Exceptions/src/main/java/com/shiv/exception/research/QueueImpl.java
@@ -0,0 +1,62 @@
+package com.shiv.exception.research;
+
+public class QueueImpl<T> {
+    private int rear;
+    private int front;
+    private int queueSize;
+    private T[] queueArray;
+
+    public QueueImpl(int queueSize){
+        this.queueSize=queueSize;
+        queueArray=(T[]) new Object[queueSize];
+        rear=-1;
+        front=-1;
+    }
+
+    public void enqueue(T data){
+        if(front==0 && rear==queueSize-1){
+            System.out.println("Queue has full");
+            return;
+        }
+        if(front==-1)
+            front=0;
+        queueArray[++rear]=data;
+    }
+
+    public void dequeue(){
+        if(front==-1){
+            System.out.println("Queue has empty");
+            return;
+        }
+        if(front>=rear){
+            /*If both pointer has same index then we need to reset the queue*/
+            front=-1;
+            rear=-1;
+        }else
+            queueArray[front++]=null;
+    }
+
+    public void show(){
+        if(front==-1){
+            System.out.println("Queue has empty");
+            return;
+        }
+        for(int i=front;i<=rear;i++)
+            System.out.print(queueArray[i]+"\t");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        QueueImpl<Integer> queue= new QueueImpl<>(4);
+        queue.enqueue(11);
+        queue.enqueue(12);
+        queue.enqueue(13);
+        queue.enqueue(14);
+        queue.show();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.show();
+    }
+}
