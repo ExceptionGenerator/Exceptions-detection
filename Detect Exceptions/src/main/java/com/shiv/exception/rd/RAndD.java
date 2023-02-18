5d9e23e1e0b97218dc8d0ee59f3168e61f689ca3
Shivmohan <nshivmohan08@gmail.com>
2023-02-18 22:59:03 +0530

    Added some R & D

---
+++ Detect Exceptions/src/main/java/com/shiv/exception/rd/RAndD.java
@@ -0,0 +1,9 @@
+package com.shiv.exception.rd;
+
+import java.util.Scanner;
+
+public class RAndD {
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(12));
+    }
+}
